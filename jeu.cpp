eddbd211156456747ad65f30df78353eac1b0d51
TooPick <alexandre-1313@hotmail.fr>
2015-03-09 21:18:20 +0100

    ajout commentaires

--- jeu.cpp
+++ jeu.cpp
@@ -54,25 +54,36 @@ using namespace std;
 
 #define ESC 27
 
+//Position de la cible
 float x = 0;
 float y = 0;
 float z = -10.0f;
+
+//Sens du mouvement de la cible
 bool sens = 0;
+
+//Vitesse de la cible
 float vitesse = 0.005f;
+
+//Vitesse de la balle
 float vitesseBalle = 0.1f;
+
+//Sensibilité de la souris
 float vSouris = 0.2f;
 
 struct balle{
 	float x,y,z;
 };
 
+//Dimensions de la fenêtre
 float width = 400;
 float height = 400;
 
 vector<balle> balles;
-vector<point3> vecteurB;
-int nbBalle = 0;
+//vector<point3> vecteurB;
+//int nbBalle = 0;
 
+//Coordonées de la caméra
 double z_eye = 5;
 
 double x_at = 0;
@@ -83,6 +94,7 @@ point3 eye(10, 5,z_eye);
 point3 at(0,0,0);
 point3 up(0,1,0);
 
+//Création de la caméra
 Camera camera(eye, at, up);
 
 GLvoid initGL()
@@ -95,13 +107,14 @@ GLvoid initGL()
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Active la correction de perspective (pour ombrage, texture, ...)
 }
 
-
+//Affiche la cible
 void affiche_paquet( float xp, float yp, float zp, float yr )
 {
-	glPushMatrix();										// Sauve la matrice de vue actuelle
-	glTranslatef(xp-2.75f, yp, zp);							// Positionne la maison avec une translation
-	glRotatef(yr, 0,0,0);								// et une rotation
+	glPushMatrix();
+	glTranslatef(xp-2.75f, yp, zp);	
+	glRotatef(yr, 0,0,0);
 
+	//Dimensions du paquet
 	GLdouble hauteurB = 2.5;
 	GLdouble hauteur = 6.3;
 	GLdouble largeur = 5.5;
@@ -181,14 +194,15 @@ void affiche_paquet( float xp, float yp, float zp, float yr )
 
 	glEnd();
 
-	glPopMatrix();										// Restaure la matrice de vue
+	glPopMatrix();
 }
 
+//Affiche une balle
 void affiche_balle( float xp, float yp, float zp, float yr )
 {
-	glPushMatrix();										// Sauve la matrice de vue actuelle
-	glTranslatef(xp, yp, zp);							// Positionne la maison avec une translation
-	glRotatef(yr, 0,1,0);								// et une rotation
+	glPushMatrix();
+	glTranslatef(xp, yp, zp);
+	glRotatef(yr, 0,1,0);
 
 	glColor3f(1.0f,1.0f,0.0f);
 
@@ -200,22 +214,25 @@ void affiche_balle( float xp, float yp, float zp, float yr )
 void affiche_scene()
 {
   //z_eye += 0.005;
+
+  //Initialisation de la caméra
   point3 neweye(0, 10, 10);
 
   point3 newat(x_at, y_at, z_at);
   camera.update(neweye, newat, up);
   camera.set();
+
+  //Affichage de la cible
 	affiche_paquet( x, y, z, 0);
 
-
+	//Affichage des balles
 	for(unsigned int i=0; i < balles.size(); i++)
 	{
 		affiche_balle(balles[i].x, balles[i].y, balles[i].z, 0);
 		//cout << "Balle : X=" << balles[i].x << " | Y=" << balles[i].y << " | Z=" << balles[i].z << endl;
 	}
 
-
-	glutSwapBuffers();							// Affiche la scï¿½ne ï¿½ l'ï¿½cran (affichage en double buffer)
+	glutSwapBuffers();
 
 }
 
@@ -258,26 +275,17 @@ void reshape(int w, int h)
 void keyboard(unsigned char key, int x, int y) {
 	switch (key) {
 
-
+	//Quitte le programme
 	case ESC:
 		exit(0);
-		break;
-
-	case 116:
-		balle b;
-		b.x = 0;
-		b.y = 0;
-		b.z = 0;
-		balles.push_back(b);
-		//cout << "Balle nï¿½" << nbBalle++ << " | X=" << b.x << " Y=" << b.y << " Z=" << b.z << endl;
 		break;
 
 	default :
 		break;
 
-}
+	}
 
-glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 void idle()
@@ -303,6 +311,7 @@ void idle()
 	//Mouvement balles
 	for(unsigned int i = 0; i < balles.size(); i++)
 	{
+		//Destruction de la balle quand elle est trop loin
 		if(balles[i].z <= -100)
 		{
 			balles.erase(balles.begin()+i);
@@ -318,24 +327,25 @@ void idle()
 
 void mouse(int button, int state, int x, int y)
 {
+	//Tire une balle avec le clique gauche
 	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
-
+		//Création de la balle sur la position de la caméra
 		balle b;
 		b.x = camera.at.x-camera.eye.x;
 		b.y = camera.at.y-camera.eye.y;
 		b.z = camera.at.z-camera.eye.z;
 		balles.push_back(b);
 
-		point3 pt = point3(camera.at.x-camera.eye.x, camera.at.y-camera.eye.y, camera.at.z-camera.eye.z);
-		vecteurB.push_back(pt);
+		//point3 pt = point3(camera.at.x-camera.eye.x, camera.at.y-camera.eye.y, camera.at.z-camera.eye.z);
+		//vecteurB.push_back(pt);
 		//cout << "Balle n°" << nbBalle++ << " | X=" << b.x << " Y=" << b.y << " Z=" << b.z << endl;
 	}
 }
 
 GLvoid callback_motion(int x, int y)
 {
-
+	//Mouvement de la caméra avec la souris
 	int w = glutGet( GLUT_WINDOW_WIDTH );
 	int h = glutGet( GLUT_WINDOW_HEIGHT );
 	if(x > w/2)
